c995f20076cb6fbc738c1ec12dfa620db4edf918
itinerant-me <itinerant.me@gmail.com>
2024-12-20 01:17:23 +0530

    fixed the package error

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
   resolve: {
     alias: {
       '@': '/src'
     }
-  },
-  optimizeDeps: {
-    include: ['react-router-dom']
   }
 })
