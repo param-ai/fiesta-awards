8093da4e40431fc9e88f1d0b0658b74ca6c0d597
itinerant-me <itinerant.me@gmail.com>
2024-12-20 01:13:42 +0530

    Your commit message describing changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  optimizeDeps: {
+    include: ['react-router-dom']
+  }
 })
